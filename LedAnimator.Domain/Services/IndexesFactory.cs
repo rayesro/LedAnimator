818331ca7eac9d2d2a97c282644dd89f53bdb0a9
ray.esro <ray.esro@gmail.com>
2023-04-24 16:49:52 -0600

    Created a Factory to generate index arrays

---
+++ LedAnimator.Domain/Services/IndexesFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace LedAnimator.Core.Domain.Services;
+
+public static class IndexesFactory
+{
+  public static int[,] GetSnakeLikeIndexFromBottomLeft(int widthParam, int heightParam)
+  {
+    var indexes = new int[widthParam, heightParam];
+    for (int y = 0; y < heightParam; y++) 
+    {
+      for (int x = 0; x < widthParam; x++)
+      {
+        int index;
+        int xCoord = widthParam*(widthParam-1-x);
+        if (x % 2 > 0)
+          index = xCoord + y;
+        else
+          index = xCoord + heightParam - 1 - y;
+        indexes[x, y] = index;
+      }
+    }
+    return indexes;
+  }
+}
---
+++ LedAnimator.Tests/Services/IndexesFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using LedAnimator.Core.Domain.Services;
+
+namespace LedAnimator.Tests.Services;
+
+public class IndexesFactoryTests
+{
+  [Test]
+  public void GivenAWidthAndHeight_WhenGettingAnIndexArray_ThenIndexValuesMustMatchAccordingly()
+  {
+    var width = 3;
+    var height = 3;
+
+    var indexes = IndexesFactory.GetSnakeLikeIndexFromBottomLeft(width, height);
+
+    Assert.That(indexes[0, 0], Is.EqualTo(8));
+    Assert.That(indexes[0, 1], Is.EqualTo(7));
+    Assert.That(indexes[0, 2], Is.EqualTo(6));
+
+    Assert.That(indexes[1, 0], Is.EqualTo(3));
+    Assert.That(indexes[1, 1], Is.EqualTo(4));
+    Assert.That(indexes[1, 2], Is.EqualTo(5));
+
+    Assert.That(indexes[2, 0], Is.EqualTo(2));
+    Assert.That(indexes[2, 1], Is.EqualTo(1));
+    Assert.That(indexes[2, 2], Is.EqualTo(0));
+  }
+}
