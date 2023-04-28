d16284f2edfe6ffb3e0fcaf1696d21689292d2bd
ray.esro <ray.esro@gmail.com>
2023-04-28 12:47:32 -0600

    Created a Index Factory class that generates a matrix of indexes for a specific Led matrix board with leds arranged as a snake shape

--- LedAnimator.Domain/Services/IndexesFactory.cs
+++ LedAnimator.Domain/Services/IndexesFactory.cs
@@ -2,22 +2,31 @@
 
 public static class IndexesFactory
 {
-  public static int[,] GetSnakeLikeIndexFromBottomLeft(int widthParam, int heightParam)
+  public static int[,] GetSnakeLikeIndexFromBottomRight(int widthParam, int heightParam)
   {
     var indexes = new int[widthParam, heightParam];
-    for (int y = 0; y < heightParam; y++) 
+
+    for (int i = heightParam - 1 , count = 0; i >= 0 ; i--)
     {
-      for (int x = 0; x < widthParam; x++)
+      for (int j = 0; j < widthParam; j++)
       {
-        int index;
-        int xCoord = widthParam*(widthParam-1-x);
-        if (x % 2 > 0)
-          index = xCoord + y;
+        int x, y;
+        if (i % 2 == 0)
+        {
+          x = j;
+          y = heightParam - 1 - i;
+          
+        }
         else
-          index = xCoord + heightParam - 1 - y;
-        indexes[x, y] = index;
+        {
+          x = widthParam - 1 - j;
+          y = heightParam - 1 - i;
+        }
+
+        indexes[x, y] = count++;
       }
     }
+    
     return indexes;
   }
 }
--- LedAnimator.Tests/Services/IndexesFactoryTests.cs
+++ LedAnimator.Tests/Services/IndexesFactoryTests.cs
@@ -10,18 +10,25 @@ public class IndexesFactoryTests
     var width = 3;
     var height = 3;
 
-    var indexes = IndexesFactory.GetSnakeLikeIndexFromBottomLeft(width, height);
+    var indexes = IndexesFactory.GetSnakeLikeIndexFromBottomRight(width, height);
 
-    Assert.That(indexes[0, 0], Is.EqualTo(8));
-    Assert.That(indexes[0, 1], Is.EqualTo(7));
-    Assert.That(indexes[0, 2], Is.EqualTo(6));
+    var expectedIndexes = new[,]
+    {
+      { 6, 7, 8 },
+      { 5, 4, 3 },
+      { 0, 1, 2 }
+    };
 
-    Assert.That(indexes[1, 0], Is.EqualTo(3));
-    Assert.That(indexes[1, 1], Is.EqualTo(4));
-    Assert.That(indexes[1, 2], Is.EqualTo(5));
+    Assert.That(indexes[0, 0], Is.EqualTo(expectedIndexes[2, 0]));
+    Assert.That(indexes[1, 0], Is.EqualTo(expectedIndexes[2, 1]));
+    Assert.That(indexes[2, 0], Is.EqualTo(expectedIndexes[2, 2]));
 
-    Assert.That(indexes[2, 0], Is.EqualTo(2));
-    Assert.That(indexes[2, 1], Is.EqualTo(1));
-    Assert.That(indexes[2, 2], Is.EqualTo(0));
+    Assert.That(indexes[0, 1], Is.EqualTo(expectedIndexes[1, 0]));
+    Assert.That(indexes[1, 1], Is.EqualTo(expectedIndexes[1, 1]));
+    Assert.That(indexes[2, 1], Is.EqualTo(expectedIndexes[1, 2]));
+
+    Assert.That(indexes[0, 2], Is.EqualTo(expectedIndexes[0, 0]));
+    Assert.That(indexes[1, 2], Is.EqualTo(expectedIndexes[0, 1]));
+    Assert.That(indexes[2, 2], Is.EqualTo(expectedIndexes[0, 2]));
   }
 }
