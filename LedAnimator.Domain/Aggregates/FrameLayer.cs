60b1f1678df42fcce79119b5cb0e9e27f1f42926
ray.esro <ray.esro@gmail.com>
2023-04-28 14:51:50 -0600

    Added classes to manage Layers

---
+++ LedAnimator.Domain/Aggregates/FrameLayer.cs
@@ -0,0 +1,10 @@
+﻿namespace LedAnimator.Core.Domain.Aggregates;
+
+public class FrameLayer
+{
+  public int Id { get; set; }
+  //Lowest order number represent the layer at the bottom of the frame
+  public int Order { get; set; }
+  public MatrixBoard Layer { get; set; }
+  public bool IsVisible { get; set; }
+}
---
+++ LedAnimator.Domain/Services/LayerManagerService.cs
@@ -0,0 +1,89 @@
+﻿using LedAnimator.Core.Domain.Aggregates;
+
+namespace LedAnimator.Core.Domain.Services;
+
+public class LayerManagerService
+{
+  private List<FrameLayer> _layers = new List<FrameLayer>();
+  private int _layerIds = 0; 
+
+  public void CreateNewLayer(MatrixBoard matrixBoardParam)
+  {
+    FrameLayer layer = new FrameLayer()
+    {
+      Id = _layerIds,
+      IsVisible = true,
+      Layer = matrixBoardParam,
+      Order = _layerIds
+    };
+    _layers.Add(layer);
+    _layerIds++;
+  }
+
+  public void RemoveLayerById(int frameLayerIdParam)
+  {
+    var layer = _layers.FirstOrDefault(x => x.Id == frameLayerIdParam);
+    if (layer != null)
+    {
+      _layers.Remove(layer);
+    }
+  }
+
+  public void SendToBack(FrameLayer layerParam)
+  {
+    _layers = _layers.OrderBy(x => x.Order).ToList();
+    var index = _layers.IndexOf(layerParam);
+    if(index == 0)
+    {
+      return;
+    }
+
+    var order = layerParam.Order;
+    layerParam.Order = _layers[index - 1].Order;
+    _layers[index - 1].Order = order;
+
+  }
+
+  public void SendBackward(FrameLayer layerParam)
+  {
+    _layers = _layers.OrderBy(x => x.Order).ToList();
+    var index = _layers.IndexOf(layerParam);
+    if (index == 0)
+    {
+      return;
+    }
+
+    var order = layerParam.Order;
+    layerParam.Order = _layers[0].Order;
+    _layers[0].Order = order;
+  }
+
+  public void BringToFront(FrameLayer layerParam)
+  {
+    _layers = _layers.OrderBy(x => x.Order).ToList();
+    var index = _layers.IndexOf(layerParam);
+    if (index == _layers.Count - 1)
+    {
+      return;
+    }
+
+    var order = layerParam.Order;
+    layerParam.Order = _layers[index + 1].Order;
+    _layers[index + 1].Order = order;
+  }
+
+  public void BringFrontward(FrameLayer layerParam)
+  {
+    _layers = _layers.OrderBy(x => x.Order).ToList();
+    var index = _layers.IndexOf(layerParam);
+    if (index == _layers.Count - 1)
+    {
+      return;
+    }
+
+    var order = layerParam.Order;
+    layerParam.Order = _layers[_layers.Count - 1].Order;
+    _layers[_layers.Count - 1].Order = order;
+  }
+
+}
