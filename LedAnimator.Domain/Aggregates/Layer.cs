8ba4b53e1b0e76ac23ebcbbb2de4349cd56b9c5a
ray.esro <ray.esro@gmail.com>
2023-04-28 15:46:45 -0600

    Renamed class

--- LedAnimator.Domain/Aggregates/Layer.cs
+++ LedAnimator.Domain/Aggregates/Layer.cs
@@ -1,10 +1,10 @@
 ﻿namespace LedAnimator.Core.Domain.Aggregates;
 
-public class FrameLayer
+public class Layer
 {
   public int Id { get; set; }
   //Lowest order number represent the layer at the bottom of the frame
   public int Order { get; set; }
-  public MatrixBoard Layer { get; set; }
+  public MatrixBoard Matrix { get; set; }
   public bool IsVisible { get; set; }
 }
